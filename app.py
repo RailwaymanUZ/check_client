66b2e61d5a26a8eda69f3c52bc8dca23ee9acc91
RailwaymanUZ <lemeshkovalerij@gmail.com>
2023-07-12 11:16:55 +0300

    generate_template

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from db import value_to_db
+from functions import make_doc, doc_to_pdf
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///client.db'
@@ -40,7 +42,7 @@ def add_client():
         try:
             db.session.add(client)
             db.session.commit()
-            return redirect('/') # перенаправляем на станичку
+            return redirect('/all_client') # перенаправляем на станичку
         except:
             return 'При додаванні клієнта виникла помилка'
 
@@ -57,12 +59,76 @@ def delete_client():
     name_company = request.form['name_company']
     number_docs = request.form['number_docs']
     company_details = request.form['company_details']
-
-    print(name_company)
-    print(number_docs)
-    print(company_details)
+    del_client = Client.query.filter_by(name_company=name_company, number_docs=number_docs,
+                                        company_details=company_details).first()
+    if del_client:
+        db.session.delete(del_client)
+        db.session.commit()
 
     return redirect('/all_client')
+
+
+@app.route('/edit_client', methods=['POST'])
+def edit_client():
+    hiden_information = request.form['hiden_information']
+    if hiden_information == 'first_vision':
+        name_company = request.form['name_company']
+        number_docs = request.form['number_docs']
+        company_details = request.form['company_details']
+        old_client = Client(name_company=name_company, number_docs=number_docs, company_details=company_details)
+
+        return render_template('edit_client.html', old_client=old_client)
+
+    if hiden_information == 'chenge':
+        old_name_company = request.form['old_name_company']
+        old_number_docs = request.form['old_number_docs']
+        old_company_details = request.form['old_company_details']
+        name_company = request.form['name_company']
+        number_docs = request.form['number_docs']
+        company_details = request.form['company_details']
+        if old_name_company != name_company or old_number_docs != number_docs or old_company_details != company_details:
+            old_client = Client.query.filter_by(name_company=old_name_company, number_docs=old_number_docs,
+                                        company_details=old_company_details).first()
+            if old_client:
+                db.session.delete(old_client)
+                db.session.commit()
+            edit_client = Client(id=1, name_company=name_company, number_docs=number_docs, company_details=company_details)
+            try:
+                db.session.add(edit_client)
+                db.session.commit()
+            except:
+                return 'При зміні клієнта виникла помилка'
+            return redirect('/all_client')
+
+@app.route('/user_templates', methods=['POST', 'GET'])
+def user_templates():
+    if request.method == "POST":
+        # получаем данные от пользователя с шаблоном и номером документа для генерации
+        gen_client = request.form['name_company']
+        gen_doc = request.form['doc']
+        gen_money = request.form['money']
+        # выбираем данные из database
+        client = Client.query.filter_by(name_company=gen_client).first()
+        gen_name = client.name_company
+        gen_number = client.number_docs
+        gen_detail = client.company_details
+        # формируем docx документ
+        make_doc(gen_doc, gen_name, gen_number, gen_detail, gen_money)
+        # формируем pdf документ
+        doc_to_pdf()
+
+        return render_template('redy_template.html')
+    else:
+        #Выбираем всех клиентов
+        client = Client.query.all()
+        '''
+        doc_file = 'Рахунок.docx'
+        doc = Document(doc_file)
+        content = []
+        for paragraph in doc.paragraphs:
+            content.append(paragraph.text)
+        '''
+        return render_template('user_templates.html', client=client)
 
 
 if __name__ == '__main__':
--- functions.py
+++ functions.py
@@ -1,6 +1,10 @@
 from docx import Document
+from docx2pdf import convert
+from datetime import datetime
+from num2words import num2words
+import pythoncom
 
-def replace_text(doc, old_text, new_text):
+def replace_text(doc, old_text, new_text):#функция заменяющая текст в файле
     for paragraph in doc.paragraphs:
         if old_text in paragraph.text:
             paragraph.text = paragraph.text.replace(old_text, new_text)
@@ -8,3 +12,50 @@ def replace_text(doc, old_text, new_text):
         for run in paragraph.runs:
             if old_text in run.text:
                 run.text = run.text.replace(old_text, new_text)
+
+    for table in doc.tables:
+        for row in table.rows:
+            for cell in row.cells:
+                for paragraph in cell.paragraphs:
+                    if old_text in paragraph.text:
+                        paragraph.text = paragraph.text.replace(old_text, new_text)
+
+                    for run in paragraph.runs:
+                        if old_text in run.text:
+                            run.text = run.text.replace(old_text, new_text)
+
+
+def make_doc(name_templates, name_company, number_doc, company_details, money):
+    path_to_doc = f'static/docs_template/{name_templates}.docx'
+    doc = Document(path_to_doc)
+    replace_text(doc, 'КОМПАНІЯ', name_company)
+    replace_text(doc, 'НОМЕР_ДОГОВОРУ', number_doc)
+    replace_text(doc, 'РЕКВІЗИТИ_КОМПАНІЇ', company_details)
+    replace_text(doc, 'СУММА', money)
+    replace_text(doc, 'ПРОПИСОМ', num2words(int(money), lang='uk'))
+    replace_text(doc, 'ДАТА', f'{datetime.today().day}.{datetime.today().month}.{datetime.today().year}')
+    replace_text(doc, 'МІСЯЦЬ', f'{datetime.today().month}')
+    replace_text(doc, 'РІК', f'{datetime.today().year}'[2:])
+    doc.save('static/docs_template/chenge.docx')
+
+
+def doc_to_pdf():
+    pythoncom.CoInitialize()
+    convert("static/docs_template/chenge.docx", "static/chenge.pdf")
+
+
+'''
+gen_name = 'ТОВ "МАМОНТ-АГРО"'
+gen_number = '387550443'
+gen_detail = 'ТОВАРИСТВО З ОБМЕЖЕНОЮ ВІДПОВІДАЛЬНІСТЮ "МАМОНТ-АГРО"\n' \
+             'Адреса: 02095, Україна, м. Київ, вул. Княжий Затон 21, прим. 566, 576\n' \
+             'Код ЄДРПОУ: 41947062\n' \
+             'ІПН: 419470626515\n' \
+             'IBAN: UA023808050000000026009597181\n' \
+             'У банку АТ «Райффайзен Банк Аваль»\n' \
+             '\n\n\nДиректор Роман Бундін'
+gen_money = '3500'
+'''
+
+#make_doc('Приклад_1', gen_name, gen_number, gen_detail, gen_money)
+#print(num2words('455.33', lang='uk'))
